5bebde90e5d639d4c50a2794e1fe77e7a1157c99
Bagheri <moh.1380.1393@gmail.com>
2022-08-01 22:55:51 +0430

    nvim

--- keymaps.lua
+++ keymaps.lua
@@ -25,6 +25,9 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
+-- Nvim_tree
+keymap("n", "<C-b>", ":NvimTreeFocus<cr>", opts)
+
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
--- lsp/configs.lua
+++ lsp/configs.lua
@@ -1,6 +1,6 @@
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not status_ok then
-  return
+	return
 end
 
 local lspconfig = require("lspconfig")
@@ -8,47 +8,48 @@ local lspconfig = require("lspconfig")
 local servers = { "jsonls", "sumneko_lua", "clangd", "tsserver", "rust_analyzer", "pyright", "cssls", "html" }
 
 lsp_installer.setup({
-  ensure_installed = servers,
+	ensure_installed = servers,
 })
 
 local opts = {
-  on_attach = require("user.lsp.handlers").on_attach,
-  capabilities = require("user.lsp.handlers").capabilities,
+	on_attach = require("user.lsp.handlers").on_attach,
+	capabilities = require("user.lsp.handlers").capabilities,
 }
 
 for _, server in pairs(servers) do
-  local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server)
-  if has_custom_opts then
-    opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
-  end
-  lspconfig[server].setup(opts)
+	local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server)
+	if has_custom_opts then
+		opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
+	end
+	lspconfig[server].setup(opts)
 end
 
-lspconfig.omnisharp.setup {
-  cmd = { "/home/bagheri/.cache/omnisharp-vim/omnisharp-roslyn/run",
-    "--languageserver", "--hostPID", tostring(vim.fn.getpid()) };
-  on_attach = require("user.lsp.handlers").on_attach,
-  capabilities = require("user.lsp.handlers").capabilities;
-}
-vim.cmd("let g:OmniSharp_highlighting = 0")vim.cmd("let g:neoformat_try_node_exe = 1")
-vim.cmd("let g:neoformat_try_node_exe = 1")
+lspconfig.omnisharp.setup({
+	cmd = {
+		"/home/bagheri/.cache/omnisharp-vim/omnisharp-roslyn/run",
+		"--languageserver",
+		"--hostPID",
+		tostring(vim.fn.getpid()),
+	},
+	on_attach = require("user.lsp.handlers").on_attach,
+	capabilities = require("user.lsp.handlers").capabilities,
+})
+
+vim.cmd("let g:OmniSharp_highlighting = 0")
+--vim.cmd("let g:neoformat_try_node_exe = 1")
+--vim.cmd("let g:neoformat_try_node_exe = 1")
 
 -- tagbar
 
 --vim.cmd("let g:tagbar_iconchars = ['   ', '   ']")
 vim.cmd("let g:tagbar_iconchars = ['  ', '  ']")
 
-
-
--- close tag
-vim.cmd("let g:closetag_filenames = \"*.html,*.xhtml,*.phtml,*.php,*.jsx,*.tsx\"")
-vim.cmd("let g:closetag_xhtml_filenames = '*.xhtml,*.jsx,*.tsx'")
-vim.cmd("let g:closetag_filetypes = 'html,xhtml,phtml'")
-vim.cmd("let g:closetag_xhtml_filetypes = 'xhtml,jsx'")
-vim.cmd("let g:closetag_regions = { 'typescript.tsx':  'jsxRegion,tsxRegion', 'javascript.jsx': 'jsxRegion','typescriptreact': 'jsxRegion,tsxRegion','javascriptreact': 'jsxRegion',}")
-vim.cmd("let g:closetag_shortcut = '>'")
-vim.cmd("let g:closetag_close_shortcut = '<leader>>'")
-vim.cmd("let g:closetag_enable_react_fragment = 1")
-
-
-
+-- close tag uncommend plug autoClose to;
+-- vim.cmd("let g:closetag_filenames = \"*.html,*.xhtml,*.phtml,*.php,*.jsx,*.tsx\"")
+-- vim.cmd("let g:closetag_xhtml_filenames = '*.xhtml,*.jsx,*.tsx'")
+-- vim.cmd("let g:closetag_filetypes = 'html,xhtml,phtml'")
+-- vim.cmd("let g:closetag_xhtml_filetypes = 'xhtml,jsx'")
+-- vim.cmd("let g:closetag_regions = { 'typescript.tsx':  'jsxRegion,tsxRegion', 'javascript.jsx': 'jsxRegion','typescriptreact': 'jsxRegion,tsxRegion','javascriptreact': 'jsxRegion',}")
+-- vim.cmd("let g:closetag_shortcut = '>'")
+-- vim.cmd("let g:closetag_close_shortcut = '<leader>>'")
+-- vim.cmd("let g:closetag_enable_react_fragment = 1")
